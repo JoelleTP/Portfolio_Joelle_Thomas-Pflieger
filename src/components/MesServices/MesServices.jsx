92e5f83fb33608641b3c21d976e2570e630eaa34
JoelleTP <joe.pflieg@hotmail.fr>
2025-08-31 15:53:34 +0200

    Addition of component "MesServices"

---
+++ src/components/MesServices/MesServices.jsx
@@ -0,0 +1,9 @@
+function MesServices() {
+    return (
+        <section id="services" className="services">
+            <h2 className="services__title">Mes services</h2>
+        </section>
+    )
+}
+
+export default MesServices
