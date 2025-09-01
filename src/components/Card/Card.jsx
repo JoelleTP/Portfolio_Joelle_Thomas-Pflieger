e7bf01867b4eea84f536d9cdadcb8ccfeea88e2b
JoelleTP <joe.pflieg@hotmail.fr>
2025-09-01 17:09:53 +0200

    Small corrections and addition of button on the project cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,18 @@
 import "./Card.scss"
 
 function Card ({ title, image }) {
+
     return (
         <div className="card">
             <img src={image} alt={title} className="card__image" />
-            <h4 className="card__title">{title}</h4>
+            <div className="card__front">
+                <h4 className="card__title">{title}</h4>
+                <button className="card__button">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </button>
+            </div>
         </div>
     )
 }
